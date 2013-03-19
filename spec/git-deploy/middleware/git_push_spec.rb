78b356a457806caebda524bfbfe628bed4ac4783
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-03-19 15:25:24 -0700

    Spec out force option of git push middleware

--- spec/git-deploy/middleware/git_push_spec.rb
+++ spec/git-deploy/middleware/git_push_spec.rb
@@ -5,15 +5,22 @@ describe Git::Deploy::Middleware::GitPush, :middleware => true do
 
   it { should be_a( Git::Deploy::Middleware ) }
 
-  before { options.stub :force? => false }
+  before( :force => true  ){ options.stub :force? => true  }
+  before( :force => false ){ options.stub :force? => false }
 
-  it 'performs the correct steps in order' do
+  it 'performs the correct steps in order', :force => false do
     step subject, :`,   'git push staging develop'
     step app,     :call, env
 
     subject.call env
   end
-  it 'returns env' do
+  it 'performs the correct steps in order when forced', :force => true do
+    step subject, :`,   'git push staging develop --force'
+    step app,     :call, env
+
+    subject.call env
+  end
+  it 'returns env', :force => false do
     subject.call( env ).should == env
   end
 end
