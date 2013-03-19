123118f0ccbefbed5c41a1c923d55f2c7bafdb98
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-03-19 15:13:43 -0700

    Add a force option to git push middleware

--- lib/git-deploy/middleware/git_push.rb
+++ lib/git-deploy/middleware/git_push.rb
@@ -1,13 +1,19 @@
 class Git::Deploy::Middleware::GitPush
   include Git::Deploy::Middleware
 
+  option :force, :type => :boolean, :default => true,
+    :desc => "I know what I'm doing"
+
   ##
   # Deploys [branch] to [remote]. Pretty much the most important thing.
   def call( env )
     remote, branch = env
 
-    # TODO force push
-    `git push #{remote} #{branch} --dry-run --quiet`
+    if options.force?
+      `git push #{remote} #{branch} --force`
+    else
+      `git push #{remote} #{branch}`
+    end
 
     app.call [ remote, branch ]
   end
