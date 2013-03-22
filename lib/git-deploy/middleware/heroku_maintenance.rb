2d2afebcc42e6627d87d2dac128f358ddd4506a6
Ben Marini <ben@remind101.com>
2013-03-21 17:07:12 -0700

    Make maintenance middlware optional

--- lib/git-deploy/middleware/heroku_maintenance.rb
+++ lib/git-deploy/middleware/heroku_maintenance.rb
@@ -1,23 +1,29 @@
 class Git::Deploy::Middleware::HerokuMaintenance
 
+  def self.used( opts )
+    opts.on :m, :maintenance, 'Turn on maintenance mode during deployment'
+  end
+
   def initialize( app )
     @app = app
   end
 
   def call( env )
+    options, remote, branch = env
+
 
     remote = Git::Deploy::Utils::Remote.new env
     heroku = Git::Deploy::Utils::Heroku.new env
 
-    heroku.maintenance_on if remote.heroku?
+    heroku.maintenance_on if options.maintenance? && remote.heroku?
 
     env = @app.call env
 
-    heroku.maintenance_off if remote.heroku?
+    heroku.maintenance_off if options.maintenance? && remote.heroku?
 
     env
   rescue Interrupt
-    heroku.maintenance_off if remote.heroku?
+    heroku.maintenance_off if options.maintenance? && remote.heroku?
 
     raise
   end
