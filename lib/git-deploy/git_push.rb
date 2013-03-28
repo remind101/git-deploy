c5da5b835bcac2694c57750f577ab4e7f3de2e5a
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-03-28 08:24:47 -0700

    Manually interrupt if a push fails

--- lib/git-deploy/git_push.rb
+++ lib/git-deploy/git_push.rb
@@ -10,6 +10,10 @@ class Git::Deploy::GitPush
 
     `git push #{env[ 'remote' ]} #{env[ 'branch' ]}`
 
+    if !$?.success?
+      raise Interrupt, 'git push failed'
+    end
+
     @app.call env
   end
 end
--- spec/git-deploy/git_push_spec.rb
+++ spec/git-deploy/git_push_spec.rb
@@ -5,9 +5,15 @@ describe Git::Deploy::GitPush, :middleware => true do
   subject { described_class.new app }
 
   it 'performs the correct steps in order' do
-    subject.should_receive( :` ).with( 'git push production feature' ).ordered
+    subject.should_receive( :` ).with( 'git push production feature' ).ordered { `test true` }
         app.should_receive( :call ).with( env ).ordered.and_call_original
 
     subject.call env
+  end
+  it 'raises an interrupt if the push fails' do
+    subject.should_receive( :` ).with( 'git push production feature' ).ordered { `test ! true` }
+        app.should_not_receive( :call )
+
+    expect { subject.call( env ) }.to raise_error( Interrupt )
   end
 end
